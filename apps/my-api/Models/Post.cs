2de704bc25c5acd89f40727985fc7ae6be94c98b
Đỗ Nhật Nam <119683629+DoNhatNam1@users.noreply.github.com>
2024-08-17 22:12:29 +0700

    .

--- apps/my-api/Models/Post.cs
+++ apps/my-api/Models/Post.cs
@@ -6,13 +6,13 @@ namespace FullstackWeb.MyApi.Models
     public class Post
     {
         public string id { get; set; } = Guid.NewGuid().ToString();
-        public string? CategoryId { get; set; }
-        public string? postSlug { get; set; }
-        public string? title { get; set; }
-        public string? shortDesc { get; set; }
-        public string? descHTML { get; set; }
-        public string? keywords { get; set; }
-        public string? siteName { get; set; }
+        public required string CategoryId { get; set; }
+        public required string postSlug { get; set; }
+        public required string title { get; set; }
+        public required string shortDesc { get; set; }
+        public required string descHTML { get; set; }
+        public required string keywords { get; set; }
+        public required string siteName { get; set; }
         public int views { get; set; } = 0;
         public DateTime createdAt { get; set; } = DateTime.UtcNow;
         public DateTime updatedAt { get; set; } = DateTime.UtcNow;
